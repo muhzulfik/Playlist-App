ab5ea345655f3f4b198d8aa7cfe7003daf17ac95
Muhamad Zulfikri <muhzulfik13@gmail.com>
2022-03-30 16:13:39 +0700

    update App and structure folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Home from "./pages";
-import Layout from "./component/Layout";
+import Layout from "./components/MainLayout";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 class Layout extends React.Component {
   render() {
