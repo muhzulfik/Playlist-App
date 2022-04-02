e8e903c8d01acf2d959e1bba1e04825e3844d006
Muhamad Zulfikri <muhzulfik13@gmail.com>
2022-04-02 19:52:14 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./pages";
+import CreatePlaylist from "./pages/Playlist/CreatePlaylist";
+import Playlist from "./pages/Playlist";
 import Layout from "./components/MainLayout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import getUserId from "./api/getuser";
+import userStore from "./store/userStore";
 
 function App() {
+  const setUserId = userStore((state) => state.setId);
+
+  useEffect(() => {
+    getUserId().then((res) => setUserId(res));
+  }, []);
+
   return (
     <ChakraProvider>
       <Layout>
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/playlist" element={<Playlist />} />
+            <Route
+              path="/playlist/createplaylist"
+              element={<CreatePlaylist />}
+            />
+          </Routes>
+        </BrowserRouter>
       </Layout>
     </ChakraProvider>
   );
